f45c125a699e77ebea0f85c4b4828c7cc7b597ab
Thiago de Oliveira <thiago.silva@ice.ufjf.br>
2023-03-09 21:06:59 -0300

    Removing the Card.vue component and implement the index count o tag span

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -10,9 +10,12 @@
         <div class="container mx-auto px-2 py-2 lg:px-32 lg:pt-12">
             <div class=" border-solid -m-1 flex flex-wrap md:-m-2">
                 <div class="flex rounded-xl m-auto">
-                    <div class="flex flex-wrap gap-8 justify-center w-full rounded-xl h-62 p-2 md:p-2 ">
-                        <Card class="cursor-pointer flex flex-col gap-2" v-for="i in 722 " :key="i"
-                            :image-src="'https://raw.githubusercontent.com/ThiagoRazor/YugiohFM-consult/main/public/imgs/' + i + '.jpg'" />
+                    <div  class="flex flex-wrap gap-8 justify-center w-full rounded-xl h-62 p-2 md:p-2 ">
+                        <div  v-for="i in 722 " :key="i" class="cursor-pointer flex flex-col gap-2"
+                             >
+                            <span class="w-2/4 text-zinc-800 text-sm text-black font-bold flex justify-center capitalize bg-zinc-400 py-2 rounded-xl">Nº: {{ i }} </span>
+                            <img class="m-auto block h-62 w-32 rounded-lg object-cover object-center" :src="'https://raw.githubusercontent.com/ThiagoRazor/YugiohFM-consult/main/public/imgs/' + i + '.jpg'" />
+                         </div>
                     </div>
                 </div>
             </div>
@@ -22,11 +25,13 @@
 
 
 <script>
-import Card from './Card.vue'
 
 export default {
-    components: {
-        Card,
+    props: {
+        imageSrc: {
+            type: String,
+            required: true,
+        }
     }
 }
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,7 +7,7 @@
     <div class="flex h-full items-center justify-center">
       <div class="px-6 text-center text-white md:px-12">
         <a href="./">
-          <img src="../assets/logo.png" class="h-24 mb-6">
+          <img src="../assets/logo2.png" class="h-24 mb-6">
         </a>
       </div>
     </div>
