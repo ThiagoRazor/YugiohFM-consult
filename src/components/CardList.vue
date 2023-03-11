988e1f2ab2f23ab95a76c84809e4bfbfcf3c0095
Thiago de Oliveira <thiago.silva@ice.ufjf.br>
2023-03-11 19:35:42 -0300

    Showing the results on the console

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -3,19 +3,21 @@
         <div class="flex justify-center">
             <div class="flex mb-3 xl:w-96">
                 <input v-model="searchCard" type="text"
+                @keydown.enter="searchCardByName()"
                     class="relative m-0 block w-full min-w-0 flex-auto rounded border border-solid border-neutral-300 bg-zinc-100 bg-clip-padding px-3 py-1.5 text-base font-normal outline-none transition duration-300 ease-in-out focus:border-primary-600 focus:text-neutral-700 focus:shadow-te-primary focus:outline-none dark:border-neutral-600 dark:text-neutral-200 placeholder:text-black"
                     placeholder="Search Card" />
             </div>
         </div>
+        <div>{{ searchCard }}</div>
         <div class="container mx-auto px-2 py-2 lg:px-32 lg:pt-12">
             <div class=" border-solid -m-1 flex flex-wrap md:-m-2">
                 <div class="flex rounded-xl m-auto">
                     <div  class="flex flex-wrap gap-8 justify-center w-full rounded-xl h-62 p-2 md:p-2 ">
-                        <div  v-for="i in 722 " :key="i" class="cursor-pointer flex flex-col gap-2"
+                        <div  v-for="i in cardInfo.length " :key="i" class="cursor-pointer flex flex-col gap-2"
                              >
                             <span class="w-2/4 text-zinc-800 text-sm text-black font-bold flex justify-center capitalize bg-zinc-400 py-2 rounded-xl">Nº: {{ i }} </span>
                             <img class="m-auto block h-62 w-32 rounded-lg object-cover object-center" :src="'https://raw.githubusercontent.com/ThiagoRazor/YugiohFM-consult/main/public/imgs/' + i + '.jpg'" />
-                         </div>
+                            </div>
                     </div>
                 </div>
             </div>
@@ -25,13 +27,31 @@
 
 
 <script>
+import cardInfo from '../cardInfo.json'
 
 export default {
-    props: {
-        imageSrc: {
-            type: String,
-            required: true,
+    data(){
+        return{
+            searchCard: '',
+            cardInfo: cardInfo,        
+            searchResult:[]
         }
+    },
+    methods:{
+        searchCardByName(){
+  this.searchResult = this.cardInfo.filter(card => {
+    return card.name.toLowerCase().includes(this.searchCard.toLowerCase());
+  })
+  console.log(this.searchResult.length)
+  var numObj = this.searchResult.length;
+
+  for (var i = 0; i < numObj; i++){
+    console.log("Id: " + this.searchResult[i].id);
+    console.log("Name: " + this.searchResult[i].name);
+    console.log("Stars:" + this.searchResult[i].price);
+  }
+},
+
     }
 }
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,4 +14,3 @@
   </div>
 </template>
 
-<style></style>
